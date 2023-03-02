ef2cb9694f5a986e15b473d1b0aaade06aa286f2
Zuxin <zuxinl@andrew.cmu.edu>
2023-03-02 15:29:47 -0500

    Update eval.py

--- script/eval.py
+++ script/eval.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', '-e', type=str, default='SafetyCarCircle-v0')
     parser.add_argument('--policy', '-p', type=str, default='robust_ppo')
-    parser.add_argument('--pretrain_dir', '-pre', type=str, default=None)
 
     parser.add_argument('--load_dir', '-d', type=str, default=None)
     parser.add_argument('--itr', '-i', type=int, default=None)
@@ -18,7 +17,6 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=str, default="cpu")
     parser.add_argument('--exp_name', type=str, default=None)
     parser.add_argument('--suffix', '--id', type=str, default=None)
-    parser.add_argument('--sleep', type=float, default=0.003)
     args = parser.parse_args()
 
     assert args.load_dir is not None, "The load_path parameter has not been specified!!!"
